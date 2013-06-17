1050f90917f629296d7f4eef35a61692b46f395d
Shanuka Gayashan <shanukagayashan@gmail.com>
2013-06-17 15:43:15 +0530

    service file

---
+++ AndroidSpring/user.php
@@ -0,0 +1,5 @@
+<?php
+header('Content-type: application/json');
+echo "{\"message\": \"success\",\"message_text\": {\"access_token\": \"1371391508|df0cef11bf1b8814ca7b99ab022fd3a2|8111\",\"expires_in\": null,\"error\": 0,\"error_message\": \"\"}}";
+
+?>
